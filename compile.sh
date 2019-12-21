524508b9ab00a38722e10d5e66491cea8b62f620
Julian M. Kunkel <juliankunkel@googlemail.com>
2019-12-21 10:06:40 +0000

    Merge lib.

--- compile.sh
+++ compile.sh
@@ -5,11 +5,11 @@ CC="${CC:-mpicc}"
 CFLAGS="-g -O2 -Wextra -Wall -pipe -std=gnu99 -Wno-format-overflow"
 LDFLAGS=""
 
-rm *.o 2>&1 || true
+rm *.o *.a 2>&1 || true
 
 echo "Building parallel find;"
 
-# Pfind can use lz4 to optimize the job stealing. 
+# Pfind can use lz4 to optimize the job stealing.
 # If you use ./prepare.sh it will try to download and compile lz4
 if [[ -e ./lz4 ]] ; then
   echo "Using LZ4 for optimization"
@@ -21,5 +21,6 @@ $CC $CFLAGS -c src/pfind-main.c || exit 1
 $CC $CFLAGS -c src/pfind-options.c || exit 1
 $CC $CFLAGS -c src/pfind.c || exit 1
 $CC $CFLAGS -o pfind *.o -lm $LDFLAGS || exit 1
+ar rcsT pfind.a pfind-options.o pfind.o $LDFLAGS
 
 echo "[OK]"
