be9c785096258a0b933dacca8d4cdf0bc245b954
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-07-06 18:36:08 +0100

    Append flags.

--- compile.sh
+++ compile.sh
@@ -9,18 +9,20 @@ rm *.o *.a 2>&1 || true
 
 echo "Building parallel find;"
 
+FILES=""
 # Pfind can use lz4 to optimize the job stealing.
 # If you use ./prepare.sh it will try to download and compile lz4
 if [[ -e ./lz4 ]] ; then
   echo "Using LZ4 for optimization"
-  CFLAGS="$CFLAGS -DLZ4 -I./lz4/lib/"
-  LDFLAGS="./lz4/lib/liblz4.a"
+  CFLAGS+=" $CFLAGS -DLZ4 -I./lz4/lib/"
+  LDFLAGS+=" ./lz4/lib/liblz4.a"
+  FILES+=" ./lz4/lib/liblz4.a"
 fi
 
 $CC $CFLAGS -c src/pfind-main.c || exit 1
 $CC $CFLAGS -c src/pfind-options.c || exit 1
 $CC $CFLAGS -c src/pfind.c || exit 1
 $CC $CFLAGS -o pfind *.o -lm $LDFLAGS || exit 1
-ar rcsT pfind.a pfind-options.o pfind.o $LDFLAGS
+ar rcsT pfind.a pfind-options.o pfind.o $FILES
 
 echo "[OK]"
