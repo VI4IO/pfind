f77c248687e89a3425af180630ea36f8821ff23b
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-10-29 11:00:26 +0000

    Fix library inclusion (for Cray/Clang LD)

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 # This script builds the optional parallel find
 
-CC="${CC:-mpicc}"
-CFLAGS="-g -O2 -Wextra -Wall -pipe -std=gnu99 -Wno-format-overflow"
-LDFLAGS=""
+CC="${CC:-gcc-10}"
+CFLAGS="-g -O2 -Wextra -Wall -pipe -std=gnu99 -Wno-format-overflow $(mpicc -showme:compile)"
+LDFLAGS="$(mpicc -showme:link)"
 
 rm *.o *.a 2>&1 || true
 
@@ -16,7 +16,7 @@ if [[ -e ./lz4 ]] ; then
   echo "Using LZ4 for optimization"
   CFLAGS+=" $CFLAGS -DLZ4 -I./lz4/lib/"
   LDFLAGS+=" ./lz4/lib/liblz4.a"
-  FILES+=" ./lz4/lib/liblz4.a"
+  FILES+=./lz4/lib/*.o
 fi
 
 $CC $CFLAGS -c src/pfind-main.c || exit 1
